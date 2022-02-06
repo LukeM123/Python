5e399fda62b29c419ed9b471a6759afdefc3de23
Luke Midgley <lukemidgley101@hotmail.co.uk>
2022-02-06 19:41:01 +0000

    Section 3. 16

--- Python/Python Course/Section 3 - Python Object and Data Structures/strings.py
+++ Python/Python Course/Section 3 - Python Object and Data Structures/strings.py
@@ -5,3 +5,18 @@ print(string[1])
 print(string[-1])
 
 print(len(string))
+
+#Substring - the right number (the stop number) is up to but not including
+print(string[1:4])
+
+#This grabs all the string
+print(string[::])
+
+#The number will be the step size. By default it is 1.
+print(string[::2])
+
+#This will start at index 2 then step every 2.
+print(string[2::2])
+
+#The will start at index 2 and end at 7. It will step every 2.
+print(string[2:7:2])
